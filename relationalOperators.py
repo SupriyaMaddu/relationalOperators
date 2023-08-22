54a3926ab4545f7b396b0e8fe81e25464c288347
Supriya Maddu <117802859+SupriyaMaddu@users.noreply.github.com>
2023-08-22 11:53:41 +0530

    Add files via upload

---
+++ relationalOperators.py
@@ -0,0 +1,53 @@
+#Greater than 70
+
+number = int(input())
+
+condition = number > 70
+print(condition)
+
+#Greatest among two numbers
+
+num1 = int(input())
+num2 = int(input())
+
+condition = num1 > num2
+print(condition) 
+
+#negative number 
+
+number = int(input())
+
+condition = number < 0
+print(condition)
+
+#equal to 
+
+W1 = input()
+W2 = input()
+
+condition = W1 == W2
+print(condition) 
+
+#not equal to 
+
+num1 = int(input())
+num2 = int(input())
+
+condition = num1 != num2
+print(condition)
+
+#Greater than or equal to 
+
+A = float(input())
+B = float(input())
+
+condition = A >= B 
+print("A >= B is " + str(condition))
+
+#less than or equal to 
+
+A = int(input())
+B = float(input())
+
+condition = A <= B 
+print(condition)
